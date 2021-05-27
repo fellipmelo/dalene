4eea1e2efc18c8f23d368775479a6a1e7b8c90f7
Fellip <43684955+fellipmelo@users.noreply.github.com>
2021-05-27 20:20:33 -0300

    Atualização - Modo grafico

---
+++ limpa.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("rm -rf /home/fellip/Dalene/monito* && rm -rf /etc/monitor && rm -rf /etc/watchpolitico.sh && rm -rf /var/log/eventos.log && rm -rf /var/log/monitoramento.log && wget https://raw.githubusercontent.com/fellipmelo/dalene/main/monitor")
